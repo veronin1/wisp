993786d079b8c9c11670f96c713fc9584aca964e
Harry Newman <h.newman2005@outlook.com>
2025-07-22 18:42:42 +0100

    Created a Vertex struct with an x,y to know the cell's location, an is_wall boolean to tell if the vertex is walkable, a visited boolean to avoid revisiting hte samevertex and a parent pointer to the previous vertex for visually reconstructing the path after solving

--- src/maze_parser.c
+++ src/maze_parser.c
@@ -2,9 +2,18 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct {
+    int x;
+    int y;
+    int is_wall;
+    int visited;
+    struct Vertex *parent;
+} Vertex;
+
 int read_file(int argc, char* argv[]) {
     FILE* maze = fopen(argv[1], "r");
     if (!maze) {
         printf("Error opening file &s\n"), strerror(errno);
     }
+
 }
