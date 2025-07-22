d7e648561d8e48726bc7ba9f4147ddb1b8256d61
Harry Newman <h.newman2005@outlook.com>
2025-07-22 18:54:48 +0100

    Better error checking

--- src/maze_parser.c
+++ src/maze_parser.c
@@ -21,9 +21,15 @@ int maze_height = 0;
 
 
 int read_file(int argc, char* argv[]) {
+    if (argv < 2) {
+        printf("Usage: %s <maze_file>\n", argv[0]);
+        return 1;
+    }
+
     FILE* maze = fopen(argv[1], "r");
     if (!maze) {
-        printf("Error opening file &s\n", strerror(errno));
+        printf("Error opening file %s: %s\n", argv[1], strerror(errno));
+        return 1;
     }
 
 }
