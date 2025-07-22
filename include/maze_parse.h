47078d98377e4197701861a2d894e3aac0d4c820
Harry Newman <h.newman2005@outlook.com>
2025-07-22 19:57:18 +0100

    Added maze_parse.h header file to define structs and overarching Maze type

---
+++ include/maze_parse.h
@@ -0,0 +1,23 @@
+#ifndef MAZE_PARSE_H
+#define MAZE_PARSE_H
+
+#define MAX_WIDTH 100
+#define MAX_HEIGHT 100
+
+typedef struct Vertex {
+    int x;
+    int y;
+    int is_wall;
+    int visited;
+    struct Vertex *parent;
+} Vertex;
+
+typedef struct {
+    Vertex grid[MAX_HEIGHT][MAX_WIDTH];
+    int width;
+    int height;
+} Maze;
+
+int load_maze(const char *filename, Maze *maze);
+
+#endif
