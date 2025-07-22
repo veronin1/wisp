5c299aa3742a05697963ccb0d7b6adebcb83ca0e
Harry Newman <h.newman2005@outlook.com>
2025-07-22 20:20:34 +0100

    Accomodated for the include/ change and Maze struct instead using pointers rather than fixed within the maze_parser.c file

--- src/maze_parser.c
+++ src/maze_parser.c
@@ -1,59 +1,43 @@
 #include <errno.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-typedef struct Vertex {
-    int x;
-    int y;
-    int is_wall;
-    int visited;
-    struct Vertex *parent;
-} Vertex;
+#include "maze_parse.h"
 
-enum {
-MAX_WIDTH = 100,
-MAX_HEIGHT = 100
-};
+int load_maze(const char* filename, Maze* maze) {
 
-Vertex maze[MAX_HEIGHT][MAX_WIDTH];
-int maze_width = 0;
-int maze_height = 0;
-
-
-int read_file(int argc, char* argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s <maze_file>\n", argv[0]);
-        return 1;
-    }
-
-
-    FILE* maze_file = fopen(argv[1], "r");
+    FILE* maze_file = fopen(filename, "r");
     if (!maze_file) {
-        printf("Error opening file %s: %s\n", argv[1], strerror(errno));
+        printf("Error opening file %s: %s\n", filename, strerror(errno));
         return 1;
     }
 
     char buffer[MAX_HEIGHT + 2]; // room for newline and null terminator
+    size_t height = 0;
+
     while (fgets(buffer, sizeof(buffer), maze_file)) {
-        int y = maze_height;
         buffer[strcspn(buffer, "\r\n")] = 0;
-        if (maze_height == 0) {
-            maze_width = strlen(buffer);
+        size_t width = strlen(buffer);
+
+        if (height == 0) {
+            maze->width = width;
         } else {
-            if (strlen(buffer) != maze_width) {
-                printf("Inconsistent line lengths in %s", argv[1]);
+            if (strlen(buffer) != maze->width) {
+                printf("Inconsistent line lengths in %s", filename);
+                return 2;
             }
         }
 
-        for (size_t x = 0, string_length = strlen(buffer); x < string_length; ++x) {
-            maze[y][x].x = x;
-            maze[y][x].y = y;
-            maze[y][x].is_wall = (buffer[x] == '#') ? 1 : 0;
-            maze[y][x].visited = 0;
-            maze[y][x].parent = NULL;
+        for (size_t col = 0, string_length = strlen(buffer); col < string_length; ++col) {
+            maze->grid[height][col].x = col;
+            maze->grid[height][col].y = height;
+            maze->grid[height][col].is_wall = (buffer[col] == '#') ? 1 : 0;
+            maze->grid[height][col].visited = 0;
+            maze->grid[height][col].parent = NULL;
         }
-        ++maze_height;
+        ++height;
     }
     fclose(maze_file);
     return 0;
