4c9911062baa7bc6577f847c7c856e8a81ccba27
Harry Newman <h.newman2005@outlook.com>
2025-07-22 18:52:14 +0100

    Defined max width, height and initialised the maze

--- src/maze_parser.c
+++ src/maze_parser.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct {
+typedef struct Vertex {
     int x;
     int y;
     int is_wall;
@@ -10,10 +10,20 @@ typedef struct {
     struct Vertex *parent;
 } Vertex;
 
+enum {
+MAX_WIDTH = 100,
+MAX_HEIGHT = 100
+};
+
+Vertex maze[MAX_HEIGHT][MAX_WIDTH];
+int maze_width = 0;
+int maze_height = 0;
+
+
 int read_file(int argc, char* argv[]) {
     FILE* maze = fopen(argv[1], "r");
     if (!maze) {
-        printf("Error opening file &s\n"), strerror(errno);
+        printf("Error opening file &s\n", strerror(errno));
     }
 
 }
