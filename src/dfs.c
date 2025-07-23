67a242b9a0afce8cb5424d7c7c89a89890ea4f85
Harry Newman <h.newman2005@outlook.com>
2025-07-23 14:50:14 +0100

    Create Vertex* neighbours

--- src/dfs.c
+++ src/dfs.c
@@ -7,7 +7,32 @@ int dfs(Maze* maze) {
   stack.data[stack.top++] = maze->start;
 
   while (stack.top != 0) {
+    Vertex* current = pop(&stack);
+    if (!current) {
+      continue;
+    }
+    if (current == maze->end) {
+      break;
+    }
+    if (!current->visited) {
+      current->visited = 1;
+    }
     for (size_t i = 0; i < 4; ++i) {
+      Vertex* nup = NULL;
+      nup->x = current->x;
+      nup->y = current->y - 1;
+
+      Vertex* ndown = NULL;
+      ndown->x = current->x;
+      ndown->y = current->y + 1;
+
+      Vertex* nleft = NULL;
+      nleft->x = current->x - 1;
+      nleft->y = current->y;
+
+      Vertex* nright = NULL;
+      nright->x = current->x + 1;
+      nleft->y = current->y;
     }
   }
   return 0;
