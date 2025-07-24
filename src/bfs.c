c7224696bc49454e0ff30f2153435794c0d7e8ed
Harry Newman <h.newman2005@outlook.com>
2025-07-24 19:24:08 +0100

    BFS Logic + queue feature implementations

--- src/bfs.c
+++ src/bfs.c
@@ -1,17 +1,42 @@
 #include "bfs.h"
 #include "maze.h"
 
+int directionX[4] = {0, 0, -1, 1};
+int directionY[4] = {-1, 1, 0, 0};
+
 int bfs(Maze* maze) {
   VertexQueue queue;
   queue.front = 0;
   queue.rear = 0;
   queue.size = 0;
-  queue.capacity = MAX_WIDTH * MAX_HEIGHT;
+  queue.capacity = (size_t)MAX_WIDTH * MAX_HEIGHT;
   queue.data[queue.rear++] = maze->start;
   queue.size++;
 
-  while (queue.size > 0) {
+  while (queue.size != 0) {
     Vertex* current = dequeue(&queue);
+
+    if (!current) {
+      continue;
+    }
+    if (current == maze->end) {
+      return 1;
+    }
+
+    for (size_t i = 0; i < 4; ++i) {
+      int neighbourX = (int)current->x + directionX[i];
+      int neighbourY = (int)current->y + directionY[i];
+
+      if (neighbourX >= 0 && neighbourX < (int)maze->width && neighbourY >= 0 &&
+          neighbourY < (int)maze->height) {
+        Vertex* neighbour = &maze->grid[neighbourY][neighbourX];
+
+        if (!neighbour->visited && !neighbour->is_wall) {
+          neighbour->visited = 1;
+          enqueue(&queue, neighbour);
+        }
+      }
+    }
   }
   return 0;
 }
@@ -25,4 +50,19 @@ Vertex* dequeue(VertexQueue* queue) {
   queue->front = (queue->front + 1) % queue->capacity;
   queue->size--;
   return front;
+}
+
+void enqueue(VertexQueue* queue, Vertex* element) {
+  if (queue->size == queue->capacity) {
+    return;
+  }
+
+  if (queue->size == 0) {
+    queue->front = queue->rear = 0;
+  } else {
+    queue->rear = (queue->rear + 1) & queue->capacity;
+  }
+
+  queue->data[queue->rear] = element;
+  queue->size++;
 }
