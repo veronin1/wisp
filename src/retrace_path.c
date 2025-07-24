882199cec52dac587502a0eff4d153d3257be151
Harry Newman <h.newman2005@outlook.com>
2025-07-25 00:27:37 +0100

    Count how many steps the maze took to solve

--- src/retrace_path.c
+++ src/retrace_path.c
@@ -1,13 +1,17 @@
 #include "retrace_path.h"
 
-void retrace_path(Vertex* end, Maze* maze) {
+size_t retrace_path(Vertex* end, Maze* maze) {
   Vertex* current = end;
+  size_t steps = 0;
   while (current != NULL && current != maze->start) {
     current->parent->on_path = 1;
     current = current->parent;
+    ++steps;
   }
 
   if (current == maze->start) {
     current->on_path = 1;
+    return steps;
   }
+  return 0;
 }
