41ca98f97807805c7c3865c7f616d3dcec4c0a37
Harry Newman <h.newman2005@outlook.com>
2025-07-25 16:30:39 +0100

    More defensive NULL checks

--- src/retrace_path.c
+++ src/retrace_path.c
@@ -1,15 +1,20 @@
 #include "retrace_path.h"
 
 size_t retrace_path(Vertex* end, Maze* maze) {
-  Vertex* current = end;
+  if (end == NULL || maze == NULL) {
+    return 0;
+  }
+
   size_t pathSteps = 0;
+  Vertex* current = end;
+
   while (current != NULL && current != maze->start) {
     current->on_path = 1;
     current = current->parent;
     ++pathSteps;
   }
 
-  if (current == maze->start) {
+  if (current != NULL && current == maze->start) {
     current->on_path = 1;
     return pathSteps;
   }
