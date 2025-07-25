ef8fbf5f5ff88a11290fd5c4d74aa4b9e20b0188
Harry Newman <h.newman2005@outlook.com>
2025-07-25 17:07:39 +0100

    Fill in maze as all walls

--- src/maze_generator.c
+++ src/maze_generator.c
@@ -9,4 +9,14 @@ void maze_generator(size_t size) {
   }
 
   maze.width = maze.height = size;
+
+  for (size_t pos_y = 0; pos_y < maze.height; ++pos_y) {
+    for (size_t pos_x = 0; pos_x < maze.width; ++pos_x) {
+      maze.grid[pos_y][pos_x].parent = NULL;
+      maze.grid[pos_y][pos_x].is_wall = 1;
+      maze.grid[pos_y][pos_x].visited = 0;
+      maze.grid[pos_y][pos_x].y = pos_y;
+      maze.grid[pos_y][pos_x].x = pos_x;
+    }
+  }
 }
