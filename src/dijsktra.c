c727e0ab00a279679e4cbcf4be168273d6603733
Harry Newman <h.newman2005@outlook.com>
2025-08-02 22:10:05 +0100

    Changed dijkstra logic to accomodate the prev and dist arrays

--- src/dijsktra.c
+++ src/dijsktra.c
@@ -29,97 +29,101 @@ int dijkstra(Maze* maze) {
         push(&heap, &maze->grid[i][j], dist[i][j]);
       }
     }
+  }
 
-    while (heap.size != 0) {
-      HeapNode minNode = extract_min(&heap);
-      Vertex* minVertex = minNode.vertex;
+  while (heap.size != 0) {
+    HeapNode minNode = extract_min(&heap);
 
-      for (size_t i = 0; i < 4; ++i) {
-        int neighbourX = (int)minVertex->x + directionX[i];
-        int neighbourY = (int)minVertex->y + directionY[i];
+    for (size_t i = 0; i < 4; ++i) {
+      int neighbourX = (int)minNode.vertex->x + directionX[i];
+      int neighbourY = (int)minNode.vertex->y + directionY[i];
 
-        if (neighbourX >= 0 && neighbourX < (int)maze->width &&
-            neighbourY >= 0 && neighbourY < (int)maze->height) {
-          Vertex* neighbourVert = &maze->grid[neighbourY][neighbourX];
-          HeapNode* tmp = &heap.data[neighbourY][neighbourX];
-          // 1 as maze is unweighted
-          int alt = minNode.distance + 1;
-          if (alt < tmp->distance) {
-          }
+      if (neighbourX >= 0 && neighbourX < (int)maze->width && neighbourY >= 0 &&
+          neighbourY < (int)maze->height) {
+        // 1 as maze is unweighted
+        int alt = minNode.distance + 1;
+        if (alt < dist[neighbourX][neighbourY]) {
+          prev[neighbourX] = minNode.vertex->x;
+          prev[neighbourY] = minNode.vertex->y;
+
+          dist[neighbourX][neighbourY] = alt;
+          // Q.decrease_priority(v, alt);
         }
       }
     }
+  }
+}
 
-    return DIJKSTRA_SUCCESS;
+return DIJKSTRA_SUCCESS;
+}
+
+HeapNode extract_min(MinHeap* heap) {
+  if (heap->size == 0) {
+    HeapNode emptyNode = {0};
+    return emptyNode;
   }
 
-  HeapNode extract_min(MinHeap * heap) {
-    if (heap->size == 0) {
-      HeapNode emptyNode = {0};
-      return emptyNode;
+  HeapNode min = heap->data[0];
+  heap->data[0] = heap->data[heap->size - 1];
+  heap->size--;
+  heapify_down(heap, 0);
+  return min;
+}
+
+void heapify_down(MinHeap* heap, size_t index) {
+  while (1) {
+    size_t leftChild = (2 * index) + 1;
+    size_t rightChild = (2 * index) + 2;
+    size_t selected = index;
+
+    if (leftChild < heap->size &&
+        heap->data[leftChild].distance < heap->data[selected].distance) {
+      selected = leftChild;
     }
 
-    HeapNode min = heap->data[0];
-    heap->data[0] = heap->data[heap->size - 1];
-    heap->size--;
-    heapify_down(heap, 0);
-    return min;
+    if (rightChild < heap->size &&
+        heap->data[rightChild].distance < heap->data[selected].distance) {
+      selected = rightChild;
+    }
+
+    if (selected == index) {
+      break;
+    }
+
+    HeapNode tmp = heap->data[selected];
+    heap->data[selected] = heap->data[index];
+    heap->data[index] = tmp;
+
+    index = selected;
+  }
+}
+
+void heapify_up(MinHeap* heap, size_t index) {
+  if (index == 0) {
+    return;
   }
 
-  void heapify_down(MinHeap * heap, size_t index) {
-    while (1) {
-      size_t leftChild = (2 * index) + 1;
-      size_t rightChild = (2 * index) + 2;
-      size_t selected = index;
-
-      if (leftChild < heap->size &&
-          heap->data[leftChild].distance < heap->data[selected].distance) {
-        selected = leftChild;
-      }
-
-      if (rightChild < heap->size &&
-          heap->data[rightChild].distance < heap->data[selected].distance) {
-        selected = rightChild;
-      }
-
-      if (selected == index) {
-        break;
-      }
-
-      HeapNode tmp = heap->data[selected];
-      heap->data[selected] = heap->data[index];
-      heap->data[index] = tmp;
-
-      index = selected;
+  while (index > 0) {
+    size_t parent = (index - 1) / 2;
+    if (heap->data[index].distance < heap->data[parent].distance) {
+      HeapNode tmp = heap->data[index];
+      heap->data[index] = heap->data[parent];
+      heap->data[parent] = tmp;
+      index = parent;
+    } else {
+      break;
     }
   }
+}
 
-  void heapify_up(MinHeap * heap, size_t index) {
-    if (index == 0) {
-      return;
-    }
-
-    while (index > 0) {
-      size_t parent = (index - 1) / 2;
-      if (heap->data[index].distance < heap->data[parent].distance) {
-        HeapNode tmp = heap->data[index];
-        heap->data[index] = heap->data[parent];
-        heap->data[parent] = tmp;
-        index = parent;
-      } else {
-        break;
-      }
-    }
+int push(MinHeap* heap, Vertex* vertex, int distance) {
+  if ((int)heap->size == MAX_HEAP_SIZE) {
+    return DIJKSTRA_FAILURE;
   }
 
-  int push(MinHeap * heap, Vertex * vertex, int distance) {
-    if ((int)heap->size == MAX_HEAP_SIZE) {
-      return DIJKSTRA_FAILURE;
-    }
-
-    heap->data[heap->size].vertex = vertex;
-    heap->data[heap->size].distance = distance;
-    heapify_up(heap, heap->size);
-    heap->size++;
-    return DIJKSTRA_SUCCESS;
-  }
+  heap->data[heap->size].vertex = vertex;
+  heap->data[heap->size].distance = distance;
+  heapify_up(heap, heap->size);
+  heap->size++;
+  return DIJKSTRA_SUCCESS;
+}
