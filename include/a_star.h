f71e7470af60fba833b323ebdb1f37f8c00b6ed3
Harry Newman <h.newman2005@outlook.com>
2025-08-04 00:11:51 +0100

    Added a_star prototypes (initial commit)

---
+++ include/a_star.h
@@ -0,0 +1,8 @@
+#ifndef A_STAR_H
+#define A_STAR_H
+
+#include "maze.h"
+
+int a_star(Maze* maze);
+
+#endif
---
+++ src/a_star.c
@@ -0,0 +1,8 @@
+#include "a_star.h"
+
+#define A_STAR_SUCCESS 1
+#define A_STAR_FAILURE 0
+
+int a_star(Maze* maze) {
+  return A_STAR_SUCCESS;
+}
