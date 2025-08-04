f6189d501e995ba48463ed785ebc13f2d3790405
Harry Newman <h.newman2005@outlook.com>
2025-08-04 21:59:55 +0100

    Added manhattan distance (the absolute value of x1, x2 + y1, y2)

--- src/a_star.c
+++ src/a_star.c
@@ -1,5 +1,10 @@
 #include "a_star.h"
+#include <stdlib.h>
+#include "maze.h"
 #include "min_heap.h"
+
+int distance[MAX_HEIGHT][MAX_WIDTH];
+int heuristic[MAX_HEIGHT][MAX_WIDTH];
 
 int a_star(Maze* maze) {
   MinHeap aStarHeap;
@@ -7,9 +12,22 @@ int a_star(Maze* maze) {
 
   for (size_t i = 0; i < maze->height; ++i) {
     for (size_t j = 0; j < maze->width; ++j) {
+      distance[i][j] = 0;
       heapPush(&aStarHeap, &maze->grid[i][j], 1);
     }
   }
 
+  int priority;
+
+  while (aStarHeap.size != 0) {
+  }
+
   return A_STAR_SUCCESS;
+}
+
+int calculate_heuristic(Vertex* start, Vertex* end) {
+  int heuristic =
+      abs((int)start->x - (int)end->x) + abs((int)start->y - (int)end->y);
+
+  return heuristic;
 }
