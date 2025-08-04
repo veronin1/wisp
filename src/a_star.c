835d4d41d2a6164f7510d9ec98c896170480d7b4
Harry Newman <h.newman2005@outlook.com>
2025-08-04 21:46:07 +0100

    Push everything onto the heap

--- src/a_star.c
+++ src/a_star.c
@@ -1,5 +1,15 @@
 #include "a_star.h"
+#include "min_heap.h"
 
 int a_star(Maze* maze) {
+  MinHeap aStarHeap;
+  aStarHeap.size = 0;
+
+  for (size_t i = 0; i < maze->height; ++i) {
+    for (size_t j = 0; j < maze->width; ++j) {
+      heapPush(&aStarHeap, &maze->grid[i][j], 1);
+    }
+  }
+
   return A_STAR_SUCCESS;
 }
