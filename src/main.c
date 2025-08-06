fc5c214c48d82e536f2a8189c3f6a9c3baa8032c
Harry Newman <h.newman2005@outlook.com>
2025-08-06 18:55:23 +0100

    Added print_maze to main

--- src/main.c
+++ src/main.c
@@ -49,6 +49,7 @@ int main(int argc, char* argv[]) {
           printf("Total Steps Taken %zu\n", totalSteps);
           printf("Found Path Steps: %zu\n", pathSteps);
           printf("=== Solution ===\n");
+          print_maze(&maze);
         }
       }
     } else {
@@ -63,6 +64,7 @@ int main(int argc, char* argv[]) {
           printf("Total Steps Taken %zu\n", totalSteps);
           printf("Found Path Steps: %zu\n", pathSteps);
           printf("=== Solution ===\n");
+          print_maze(&maze);
         }
       } else {
         printf("No path found\n");
@@ -79,6 +81,7 @@ int main(int argc, char* argv[]) {
           printf("Total Steps Taken %zu\n", totalSteps);
           printf("Found Path Steps: %zu\n", pathSteps);
           printf("=== Solution ===\n");
+          print_maze(&maze);
         }
       } else {
         printf("No path found\n");
